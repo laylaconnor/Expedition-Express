1ec21e4ed697ca632d63565ea3a21583d73cee01
laylaconnor <layla.connor@yahoo.com>
2025-04-11 17:09:09 -0400

    Finished logout and personalized welcome message

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'em
 app.get('/auth/google/callback', 
     passport.authenticate('google', { failureRedirect: '/' }), 
     (req, res) => {
-        res.redirect(`http://localhost:8080/index.html?token=${req.user.id}`);
+        res.redirect(`http://localhost:8080/index.html?token=${req.user.id}&displayName=${encodeURIComponent(req.user.displayName)}`);
     }
 );
 
